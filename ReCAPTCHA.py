2d451dbb4950286940521825ea10f669993515fa
Pingu Lee <escapeweedy@gmail.com>
2024-08-06 17:03:07 +0900

    무한 루프 했을 때 오류 해결

--- ReCAPTCHA.py
+++ ReCAPTCHA.py
@@ -7,7 +7,7 @@ import time
 target_image_path = 'test.png'
 
 # 대기 시간 설정 (초)
-wait_time = 0.1  # 0.3초마다 이미지를 찾음
+wait_time = 0.1  # 0.1초마다 이미지를 찾음
 
 def find_and_click_image(image_path):
     # 대상 이미지 로드
@@ -31,6 +31,7 @@ def find_and_click_image(image_path):
                 target_x, target_y = target_loc[0] + target_width // 2, target_loc[1] + target_height // 2
                 pyautogui.moveTo(target_x, target_y)
                 pyautogui.click()
+                break  # 이미지를 찾고 클릭한 후에 루프를 종료합니다.
 
             time.sleep(wait_time)
 
